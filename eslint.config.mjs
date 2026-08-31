5d6008c240ffa4f2af13e55225b42f3489dafdc5
yyxi <178086254+yyxi@users.noreply.github.com>
2026-08-31 10:01:36 +0000

    chore: upkeep

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,30 @@
 // @ts-check
 import { escapace, compose } from 'eslint-config-escapace'
 
-export default compose(escapace(), {
-  rules: {
-    'depend/ban-dependencies': ['error', { allowed: ['find-up'] }],
-    'unicorn/prevent-abbreviations': [
-      'error',
-      {
-        allowList: {
-          fromTSDocNode: true,
-          FromTSDocNodeOptions: true,
-          isTSDocNodeKind: true,
-          TSDocMarkdownWriter: true,
-          TSDocMarkdownWriterOptions: true,
-          TSDocMarkdownWriterState: true,
-        },
-      },
-    ],
+export default compose(
+  escapace(),
+  {
+    rules: {
+      'depend/ban-dependencies': ['error', { allowed: ['find-up'] }],
+    },
   },
-})
+  {
+    files: ['**/*.?([cm])[jt]s?(x)'],
+    rules: {
+      'unicorn/name-replacements': [
+        'error',
+        {
+          allowList: {
+            fromTSDocNode: true,
+            FromTSDocNodeOptions: true,
+            isTSDocNodeKind: true,
+            TSDocMarkdownWriter: true,
+            TSDocMarkdownWriterOptions: true,
+            TSDocMarkdownWriterState: true,
+          },
+          replacements: { configuration: false },
+        },
+      ],
+    },
+  },
+)
